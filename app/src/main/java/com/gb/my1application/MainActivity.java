6df3072f1fcd18594877f4849afab2bae1552cd0
Pasha <alkatrazzzzz@mail.ru>
2021-09-12 22:10:32 +0300

    Дописал логику

--- app/src/main/java/com/gb/my1application/MainActivity.java
+++ app/src/main/java/com/gb/my1application/MainActivity.java
@@ -6,29 +6,44 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-    SharedPreferences settings;
+    SharedPreferences profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.submit_button);
         button.setOnClickListener(view -> {
-            EditText name = findViewById(R.id.name);
-            EditText surname = findViewById(R.id.surname);
-            EditText age = findViewById(R.id.age);
-            settings = getSharedPreferences("Profile", MODE_PRIVATE);
-            SharedPreferences.Editor prefEditor = settings.edit();
-//            prefEditor.putString()=
-                    collectData(name, surname, age);
-
-            Intent switcher= new Intent(MainActivity.this, SecondaryActivity.class);
+            profile = getSharedPreferences("Profile", MODE_PRIVATE);
+            EditText nameEditText = findViewById(R.id.name);
+            EditText surnameEditText = findViewById(R.id.surname);
+            EditText ageEditText = findViewById(R.id.age);
+            ToggleButton tb1ToggleButton = findViewById(R.id.tb1);
+            ToggleButton tb2ToggleButton = findViewById(R.id.tb2);
+            CheckBox checkBox = findViewById(R.id.checkbox1);
+            CalendarView calendarView = findViewById(R.id.calendar);
+            collectData(nameEditText, surnameEditText, ageEditText, tb1ToggleButton,
+                    tb2ToggleButton, checkBox, calendarView);
+            Intent switcher = new Intent(MainActivity.this, SecondaryActivity.class);
             MainActivity.this.startActivity(switcher);
         });
     }
 
-    private void collectData(EditText name, EditText surname, EditText age) {
+    private void collectData(EditText name, EditText surname, EditText age, ToggleButton tb1,
+                             ToggleButton tb2, CheckBox checkBox, CalendarView calendarView) {
+        SharedPreferences.Editor prefEditor = profile.edit();
+        prefEditor.putString("Name", name.toString());
+        prefEditor.putString("Surname", surname.toString());
+        prefEditor.putInt("Birthday", calendarView.getDateTextAppearance());
+        prefEditor.putString("Age", age.toString());
+        prefEditor.putBoolean("Subscribed", tb1.isChecked());
+        prefEditor.putBoolean("Insured", tb2.isChecked());
+        prefEditor.putBoolean("Permission", checkBox.isChecked());
     }
 }
--- app/src/main/java/com/gb/my1application/SecondaryActivity.java
+++ app/src/main/java/com/gb/my1application/SecondaryActivity.java
@@ -2,13 +2,28 @@ package com.gb.my1application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondaryActivity extends AppCompatActivity {
-
+    SharedPreferences profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        Button button = findViewById(R.id.final_button);
+        button.setOnClickListener(view -> {
+            EditText phoneEditText = findViewById(R.id.phone_num);
+            EditText mailEditText = findViewById(R.id.e_mail);
+            EditText passwordEditText = findViewById(R.id.password);
+            profile = getSharedPreferences("Profile", MODE_PRIVATE);
+            SharedPreferences.Editor prefEditor = profile.edit();
+            prefEditor.putString("Phone", phoneEditText.toString());
+            prefEditor.putString("Mail", mailEditText.toString());
+            prefEditor.putString("Password", passwordEditText.toString());
+        });
+
     }
 }
